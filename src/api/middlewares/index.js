d8f6ba0e7dd93b8172ff0b1ac7f62e339a2f64d2
Milan Timotijevic <milantimotijevic@Milans-MacBook-Pro.local>
2020-11-27 15:15:52 +0100

    update

--- src/api/middlewares/index.js
+++ src/api/middlewares/index.js
@@ -7,11 +7,23 @@ const errorHandler = require('./errorHandler');
 
 const isProdEnv = process.env.NODE_ENV === 'production';
 const prodMiddlewares = [compression(), helmet()];
+
+const whitelist = ['http://hetzner-test:3001']
+const corsOptions = {
+	origin: (origin, callback) => {
+		if (whitelist.indexOf(origin) !== -1 || !origin) {
+			callback(null, true)
+		} else {
+			callback(new Error('Not allowed by CORS'))
+		}
+	}
+}
+
 const middlewares = [
   ...(isProdEnv ? prodMiddlewares : []),
   bodyParser.json(),
   bodyParser.urlencoded({ extended: true }),
-  cors(),
+  cors(corsOptions),
   morgan('dev'),
 ];
 
