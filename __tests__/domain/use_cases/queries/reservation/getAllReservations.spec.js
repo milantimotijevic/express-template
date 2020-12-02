4cca2d61aaaa31ba512f3e5af264da8a359ce9c5
Milan Timotijevic <milantimotijevic@Milans-MacBook-Pro.local>
2020-12-02 13:15:58 +0100

    add tests

---
+++ __tests__/domain/use_cases/queries/reservation/getAllReservations.spec.js
@@ -0,0 +1,18 @@
+const getAllReservations = require('../../../../../src/domain/use_cases/queries/reservation/getAllReservations');
+const ReservationRepository = require('../../../../../src/repository/ReservationRepository');
+
+jest.mock('../../../../../src/repository/ReservationRepository');
+
+describe('getAllReservations tests', () => {
+	test('getAllReservations returns a list of reservations', async () => {
+		const mocked = {
+			guestName: 'Milan',
+			hotelName: 'Some Hotel',
+		};
+
+		ReservationRepository.getAllReservations.mockResolvedValue(mocked);
+		const result = await getAllReservations();
+
+		expect(result).toMatchObject(mocked);
+	});
+});
